ad3c9d3b489ffe2d0383b625a6a7a59518235dc6
Ishan Chhabra <ishan.chhabra@hotmail.com>
2020-04-23 05:16:07 +0530

    feat(server): add link helpers
    Signed-off-by: Ishan Chhabra <ishan.chhabra@hotmail.com>

---
+++ api/utils/link-helpers.ts
@@ -0,0 +1,13 @@
+/**
+ * Generates a random code of given length
+ * @param length length of random code
+ */
+export const generateRandomCode = (length: number = 5) => {
+  var result = "";
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
