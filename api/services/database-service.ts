c7c98196db57ede8c5c48e6c501fc7ded03152b7
Ishan Chhabra <ishan.chhabra@hotmail.com>
2020-04-23 05:10:31 +0530

    chore(server): add database service
    Signed-off-by: Ishan Chhabra <ishan.chhabra@hotmail.com>

---
+++ api/services/database-service.ts
@@ -0,0 +1,46 @@
+import * as MongoDB from "mongodb";
+import { LoggerService } from "../services/logger-service";
+
+/**
+ * Drives a database connection in a singleton.
+ */
+export class DatabaseService {
+  public database: MongoDB.Db | null = null;
+
+  private client: MongoDB.MongoClient = new MongoDB.MongoClient(
+    process.env.DB_URL || ""
+  );
+  private static instance: DatabaseService;
+
+  constructor() {
+    this.initialize();
+  }
+
+  /**
+   * Initializes the database
+   */
+  private async initialize(): Promise<any> {
+    try {
+      const client = await this.client.connect();
+      this.database = client.db(process.env.DB_NAME);
+      LoggerService.getInstance().LOGGER.info(
+        `Established connection to database <${this.database.databaseName}>`
+      );
+    } catch (error) {
+      LoggerService.getInstance().LOGGER.error(
+        `Failed to established connection to database <${process.env.DB_NAME}>`
+      );
+    }
+  }
+
+  /**
+   * The static method that controls the access to the DatabaseService instance.
+   */
+  public static getInstance(): DatabaseService {
+    if (!DatabaseService.instance) {
+      DatabaseService.instance = new DatabaseService();
+    }
+
+    return DatabaseService.instance;
+  }
+}
