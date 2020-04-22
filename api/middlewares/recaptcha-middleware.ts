2dc7cf77ef418f62adf5c397d10baa6ed6d5f888
Ishan Chhabra <ishan.chhabra@hotmail.com>
2020-04-23 05:20:37 +0530

    feat(server): add ReCAPTCHA middleware
    Signed-off-by: Ishan Chhabra <ishan.chhabra@hotmail.com>

---
+++ api/middlewares/recaptcha-middleware.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "Express";
+import axios from "axios";
+import { ErrorMessage } from "../constants";
+
+/**
+ * The ReCAPTCHA middleware
+ * @param req an Express Request object
+ * @param res an Express Response object
+ * @param next the Next function
+ */
+export const recaptchaMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const url = "https://www.google.com/recaptcha/api/siteverify";
+
+  const result = await axios.post(
+    url,
+    {},
+    {
+      params: {
+        secret: process.env.RECAPTCHA_SECRET,
+        response: req.headers["x-recaptcha-token"],
+      },
+    }
+  );
+  if (result.data.success) return next();
+
+  return res.status(ErrorMessage.Recaptcha.INVALID_RECAPTCHA.code).json({
+    success: false,
+    message: ErrorMessage.Recaptcha.INVALID_RECAPTCHA.error,
+  });
+};
