5ffc14eee5c1f7b93ca732bce3d2fff6ee4ed27a
Ishan Chhabra <ishan.chhabra@hotmail.com>
2020-04-23 05:18:39 +0530

    feat(server): create constants
    Signed-off-by: Ishan Chhabra <ishan.chhabra@hotmail.com>

---
+++ api/constants.ts
@@ -0,0 +1,68 @@
+/**
+ * Defines Constants for API
+ */
+export const Constants = {
+  PORT: process.env.PORT || 3000,
+  TTLSeconds: Number(process.env.TTL_SECONDS) || 60 * 60 * 6,
+};
+
+/**
+ * Defines Endpoints for API
+ */
+export const APIEndpoints = {
+  BASE: "/api/v1",
+  Links: {
+    BASE_ENDPOINT: "/links",
+    CREATE_LINK: "/",
+    GET_LINK: "/:shortCode([A-Za-z]{5})",
+    MY_LINKS: "/me",
+  },
+  Analytics: {
+    BASE_ENDPOINT: "/analytics",
+    FETCH_ANALYTICS: "/:analyticsCode([A-Za-z]{5})",
+  },
+};
+
+/**
+ * Defines Collection names in database
+ */
+export const Collections = {
+  LINKS: "links",
+};
+
+/**
+ * Defines properties for error messages
+ */
+export const ErrorMessage = {
+  Recaptcha: {
+    INVALID_RECAPTCHA: {
+      code: 401,
+      error: "ERR_INVALID_RECAPTCHA",
+      log: "ERR_INVALID_RECAPTCHA",
+    },
+  },
+  RateLimit: {
+    RATE_LIMIT_EXCEEDED: {
+      code: 429,
+      error: "ERR_RATE_LIMIT_EXCEEDED",
+      log: "ERR_RATE_LIMIT_EXCEEDED",
+    },
+  },
+  Link: {
+    INVALID_ID_TOKEN: {
+      code: 401,
+      error: "INVALID_ID_TOKEN",
+      log: "INVALID_ID_TOKEN",
+    },
+    EXPIRED_ID_TOKEN: {
+      code: 401,
+      error: "EXPIRED_ID_TOKEN",
+      log: "EXPIRED_ID_TOKEN",
+    },
+    INVALID_TICKET: {
+      code: 401,
+      error: "INVALID_TICKET",
+      log: "INVALID_TICKET",
+    },
+  },
+};
