325350437f74583f8cc83c39849a164baf841f5a
Ritvick-me <pandey.ritvick.v@gmail.com>
2020-05-26 16:59:37 +0530

    feat(api-calling): calling api

--- client/src/components/table.svelte
+++ client/src/components/table.svelte
@@ -15,11 +15,6 @@
         position: relative;
     }
 
-    .kz-qr-modal-body{
-        height: 100vh;
-        position: relative;
-    }
-
     .kz-absolute{
         position: absolute;
         top: 0;
@@ -95,6 +90,50 @@
         margin-top:15px;
     }
 
+    input[type="checkbox"].kz-switch{
+        position: relative;
+        margin-right: 10px;
+        width: 40px;
+        height: 20px;
+        appearance: none;
+        -webkit-appearance: none;
+        background-color: #ffffff;
+        outline: none;
+        border-style: solid;
+        border-color: #ffffff;
+        border-width: 2px;
+        border-radius: 5px;
+        box-shadow: inset 0 0 5px #ffffff;
+        transition: 0.5s;
+    }
+
+    /* input:checked[type="checkbox"].kz-switch{
+        background-color: #000000;
+    } */
+
+    input[type="checkbox"].kz-switch:before{
+        position: absolute;
+        content: '';
+        padding-left: 4px;
+        width: 20px;
+        height: 20px;
+        border-radius: 5px;
+        transform: scale(0.8);
+        top: -2px;
+        left: 0;
+        background-color: #f5f5f5;
+        transition: 0.5s;
+    }
+
+    input:checked[type="checkbox"].kz-switch:before{
+        left: 19px;
+        transform: scale(0.9);
+        background-color: #000000;
+        content: '\2713';
+        color: #ffffff;
+        /* font-weight: 999; */
+    }
+
     @media(max-width: 640px){
         .kz-input{
             min-width: 40%;
@@ -167,6 +206,7 @@ function hideQREditor(){
     {count}
 </div>
 <div class="col col-3 kz-finder">
+    <input class="kz-switch" type="checkbox" name="">
     <svg on:click={showEditor} style="padding-right: 5px;" height="20px" width="20px" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pen" class="svg-inline--fa fa-pen fa-w-16 kz-edit-btn" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M290.74 93.24l128.02 128.02-277.99 277.99-114.14 12.6C11.35 513.54-1.56 500.62.14 485.34l12.7-114.22 277.9-277.88zm207.2-19.06l-60.11-60.11c-18.75-18.75-49.16-18.75-67.91 0l-56.55 56.55 128.02 128.02 56.55-56.55c18.75-18.76 18.75-49.16 0-67.91z"></path></svg>
     <svg style="padding-right: 5px;" height="24px" viewBox="0 0 29 34" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
         <!-- Generator: Sketch 64 (93537) - https://sketch.com -->
