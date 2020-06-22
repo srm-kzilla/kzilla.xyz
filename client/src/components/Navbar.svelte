0c1c62b8972b37e2b9b902dd0c957106eabffeec
harshgoel05 <harshgoel05@gmail.com>
2020-06-22 15:16:50 +0530

    feat(analytics): analytics page added

--- client/src/components/Navbar.svelte
+++ client/src/components/Navbar.svelte
@@ -1,17 +1,20 @@
 <script>
-    import { Router, Link, Route } from "svelte-routing";
-    import HomePage from '../homePage/home.svelte';
-    import myLinks from '../myLinksPage/myLinks.svelte';
-    
-    export let url = "";
-    console.log(url);
+  import { Router, Link, Route } from "svelte-routing";
+  import HomePage from "../homePage/home.svelte";
+  import myLinks from "../myLinksPage/myLinks.svelte";
+  import Analytics from "../analyticsPage/analytics.svelte";
+
+  export let url = "";
+  console.log(url);
 </script>
 
-<Router url="{url}">
-    <div style="z-index: 999">
-        <Route path="myLinks" component="{myLinks}" />
-        <Route path="/"><HomePage/></Route>
-        <Route path="analytics/:id" component="{myLinks}" />
-    </div>
+<Router {url}>
+  <div style="z-index: 999">
+    <Route path="myLinks" component={myLinks} />
+    <Route path="/">
+      <HomePage />
+    </Route>
+    <Route path="analytics/:id" component={Analytics} />
+  </div>
 </Router>
-<br>
+<br />
