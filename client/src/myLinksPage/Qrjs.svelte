1186489115f78ed59ddb97bcfbadb5017f1f3252
Vaishnav <84540554+vaishnav-mk@users.noreply.github.com>
2022-10-07 05:45:01 +0530

    Update Qrjs.svelte
    Updated the cdn address for qrcodejs

--- client/src/myLinksPage/Qrjs.svelte
+++ client/src/myLinksPage/Qrjs.svelte
@@ -1,41 +1,36 @@
 <script>
-  import { onMount } from 'svelte';
-	
+	import { onMount } from 'svelte';
+
 	export let codeValue;
-	export let squareSize; 
-	
-  let qrcode;
-			
+	export let squareSize;
+
+	let qrcode;
+
 	onMount(() => {
-
 		let script = document.createElement('script');
-    script.src = "https://cdn.rawgit.com/davidshimjs/qrcodejs/gh-pages/qrcode.min.js";
-    document.head.append(script);
-	
-		script.onload = function() {
+		script.src = 'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js';
+		document.head.append(script);
 
+		script.onload = function () {
 			qrcode = new QRCode(codeValue, {
 				text: codeValue,
-		  	width: squareSize,
-        height: squareSize,
-        colorDark : "#000000",
-        colorLight : "#ffffff",
-        correctLevel : QRCode.CorrectLevel.H
-			});		
-			
-    };
-  });
-  
+				width: squareSize,
+				height: squareSize,
+				colorDark: '#000000',
+				colorLight: '#ffffff',
+				correctLevel: QRCode.CorrectLevel.H
+			});
+		};
+	});
 </script>
 
+<div class="qrcode" id={codeValue} />
+
 <style>
-  .qrcode {
-    width:250px;
-    height:300px;
-    margin-top:15px;
-    margin: auto;
-  }
+	.qrcode {
+		width: 250px;
+		height: 300px;
+		margin-top: 15px;
+		margin: auto;
+	}
 </style>
-
-<div class="qrcode" id="{codeValue}"></div>
-
